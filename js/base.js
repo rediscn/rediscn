75a960951c67a88fe9ff7044bba463a2d05072f3
rjgcs123 <geelou@qq.com>
2016-05-19 14:40:24 +0800

    添加首页banner图片

--- js/base.js
+++ js/base.js
@@ -1,3 +1,9 @@
+$.getUrlParam = function (name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]); return null;
+};
+
 /**
  * 判断是否是移动浏览器
  */
